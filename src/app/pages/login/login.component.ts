25bb895a2ac2e308cde930b9ab64c92048fc2354
ilja115610 <57315435+ilja115610@users.noreply.github.com>
2021-07-06 16:39:07 +0300

    login done + validation

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -17,28 +17,55 @@ export class LoginComponent implements OnInit {
 
   message: string;
 
+  timer: boolean;
+
   constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
 
-  getLogin () {
+  getLogin (form) {
+
+    if(this.validateForm(form)==='invalid'){
+      return;
+    }
 
     let response = this.loginService.getAuthentication(this.login, this.password);
     response.subscribe(data=>{
       this.message = data.message;
       this.isAdmin = data.admin;
-      console.log(this.message, this.isAdmin)
       if(this.isAdmin){
         this.router.navigate(["/admin"])
       }
       else {
         this.router.navigate(["/products"])
       }
+    },()=>{
+      this.message = "Wrong username or password"
     })
 
   }
 
+  setTimer () {
+    this.timer = true;
+    setTimeout(()=>{
+      this.timer = false;
+    },3000)
+  }
+
+
+  validateForm (form) {
+
+    form.controls.login.touched = true;
+    form.controls.password.touched = true;
+
+    if(this.login === undefined || this.login.length<4){
+      return 'invalid';
+    }
+    if(this.password === undefined || this.password.length<4){
+      return 'invalid';
+    }
+  }
 
 }
