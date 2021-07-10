301e1900cf541142143a26825e2823cdf22c1925
Kirjanq4 <kirilld90.kd@gmail.com>
2021-07-10 12:26:14 +0300

    order finished, UI fixed

--- src/app/purchase/purchase.component.ts
+++ src/app/purchase/purchase.component.ts
@@ -1,32 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 
 import { CartService } from '../services/cart.service';
-import {Purchase} from '../common/purchase';
-import {PurchaseService} from '../services/purchase.service';
+import { Purchase } from '../common/purchase';
+import { PurchaseService } from '../services/purchase.service';
+import { OrderItem } from '../common/order-item';
+import { PurchaseResponse } from '../common/purchase-response';
 
 @Component({
   selector: 'app-purchase',
   templateUrl: './purchase.component.html',
-  styleUrls: ['./purchase.component.sass'],
+  styleUrls: ['./purchase.component.css'],
 })
 export class PurchaseComponent implements OnInit {
-
-
   purchase: Purchase = new Purchase();
+
+  purchaseRespone: PurchaseResponse = new PurchaseResponse();
+
+  orderItem: OrderItem = new OrderItem();
 
   message: string;
 
-
-
-
-
   items = this.cartService.getItems();
 
-  constructor(private cartService: CartService, private purchaseService: PurchaseService) {}
+  address: string = '';
+
+  constructor(
+    private cartService: CartService,
+    private purchaseService: PurchaseService
+  ) {}
 
   ngOnInit(): void {}
 
-  getTotalPrice() {}
   totalPrice = this.items.reduce((prev, cur) => prev + cur.price, 0);
-}
 
+  sendPurchase() {
+    this.purchase.totalPrice = this.totalPrice;
+    this.purchase.totalQuantity = this.items.length;
+    this.purchase.deliveryAddress = this.address;
+
+    this.items.map((item) => {
+      let arr = new OrderItem();
+      arr.productId = item.id;
+      arr.quantity = 1;
+      arr.unitPrice = item.price;
+
+      this.purchase.orderItems.push(arr);
+    });
+
+    console.log(this.purchase);
+    this.purchaseRespone.message = 'Thank You';
+    alert(this.purchaseRespone.message);
+
+    this.purchaseService.setPurchase(this.purchase);
+  }
+}
